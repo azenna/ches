26625ab4dd63bbcff6d4141f4fe85d8e31d1a076
wolfy752 <isallwein@gmial.com>
2024-11-24 22:55:32 -0600

    init and print board

--- main.go
+++ main.go
@@ -5,39 +5,68 @@ import "fmt"
 type Color int
 
 const (
-	Black Color = iota
+	NoColor Color = iota
 	White
-	NoColor
+	Black
 )
 
 type PieceType int
 
 const (
-	Pawn PieceType = iota
+	NoPiece PieceType = iota
 	Rook
 	Knight
 	Bishop
 	Queen
 	King
-	NoPiece
+	Pawn
 )
 
 type Piece struct {
 	piecetype PieceType
 	color     Color
 }
- EmptySquare = Piece {NoPiece,NoColor}
 
-func main() {
-	fmt.Println("Hello world")
-	fmt.Println("Hello world")
-
-	for _ = range 10 {
-		fmt.Println("in for loop")
-
-	}
-}
+var EmptySquare = Piece{NoPiece, NoColor}
 
 type Board struct {
 	state [8][8]Piece
+}
+
+func PrintBoard(board Board) {
+	for _, row := range board.state {
+		for _, piece := range row {
+			PrintPiece(piece)
+		}
+		fmt.Println()
+	}
+
+}
+func PrintPiece(piece Piece) {
+	s := " "
+	if piece.color == White {
+		s = "W"
+	} else if piece.color == Black {
+		s = "B"
+	}
+	t := " "
+	switch piece.piecetype {
+	case Pawn:
+		t = "P"
+	case Rook:
+		t = "R"
+	case Knight:
+		t = "N"
+	case Bishop:
+		t = "B"
+	case King:
+		t = "K"
+	case Queen:
+		t = "Q"
+	}
+	fmt.Printf("[%s%s]", s, t)
+}
+func main() {
+	board := Board{[8][8]Piece{}}
+	PrintBoard(board)
 }
